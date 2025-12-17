63a791daf30f3bc9a86980fc0e967abd007a6682
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-17 20:41:28 -0300

    CategoryResource

---
+++ backend/src/main/java/com/devsuprior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,23 @@
+package com.devsuprior.dscatalog.resources;
+
+import com.devsuprior.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Controla as requisições da api
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Eletronics"));
+        return ResponseEntity.ok().body(list);
+    }
+}
