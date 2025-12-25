24cb140b02ec41476c0b1c0eae52b04c0602580f
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-25 16:48:05 -0300

    POST new Category

--- backend/src/main/java/com/devsuprior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuprior/dscatalog/resources/CategoryResource.java
@@ -1,16 +1,13 @@
 package com.devsuprior.dscatalog.resources;
 
 import com.devsuprior.dscatalog.dto.CategoryDTO;
-import com.devsuprior.dscatalog.entities.Category;
 import com.devsuprior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 //Controla as requisições da api
@@ -34,5 +31,13 @@ public class CategoryResource {
         // O list está buscando os registros da classe CategoryService pelo método findAll
        CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
