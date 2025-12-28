31384ce7094b47c0fa13ef790554d6ebfcd257da
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-12-27 21:31:47 -0300

    Auditing data

--- backend/src/main/java/com/devsuprior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuprior/dscatalog/services/CategoryService.java
@@ -55,15 +55,14 @@ public class CategoryService {
         return new CategoryDTO(entity);
     }
 
-    @Transactional(readOnly = true)
-    public CategoryDTO update(Long id,CategoryDTO dto) {
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
         try {
             Category entity = repository.getReferenceById(id);
             entity.setName(dto.getName());
             entity = repository.save(entity);
             return new CategoryDTO(entity);
-
-        }catch (EntityNotFoundException e){
+        } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
         }
     }
