e913eb22b18d35f319ccda84d75e414f093f806f
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-01-17 20:57:33 -0300

    Testando save com id nulo

--- backend/src/test/java/com/devsuprior/dscatalog/repositories/ProductRepositoryTest.java
+++ backend/src/test/java/com/devsuprior/dscatalog/repositories/ProductRepositoryTest.java
@@ -1,12 +1,12 @@
 package com.devsuprior.dscatalog.repositories;
 
 import com.devsuprior.dscatalog.entities.Product;
+import com.devsuprior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -18,11 +18,25 @@ public class ProductRepositoryTest {
 
     private long existing;
     private long nonExistingId;
+    private long countTotalProducts;
 
     @BeforeEach
     void setUp() throws Exception{
         existing = 1L;
         nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull(){
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
     }
 
     @Test
@@ -38,14 +52,6 @@ public class ProductRepositoryTest {
         //●	Assert: declare o que deveria acontecer (resultado esperado)
         Optional<Product> result = repository.findById(existing);
         Assertions.assertFalse(result.isPresent());
-    }
-
-    @Test
-    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-
-        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-            repository.deleteById(nonExistingId);
-        });
     }
 
 }
