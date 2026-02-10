b0b4037bb81f460c82ab05f371e3f2a657eb6d79
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-02-09 21:35:07 -0300

    Testando o update

--- backend/src/test/java/com/devsuprior/dscatalog/resources/ProductResourceTest.java
+++ backend/src/test/java/com/devsuprior/dscatalog/resources/ProductResourceTest.java
@@ -14,11 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,6 +38,10 @@ public class ProductResourceTest {
     // substituindo o bean real. Isso permite simular o comportamento do serviço.
     @MockitoBean
     private ProductService service;
+
+    // ObjectMapper: Utilitário para converter objetos Java em JSON e vice-versa.
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private Long existingId;
 
@@ -69,6 +76,57 @@ public class ProductResourceTest {
         
         // Configura o mock para lançar exceção quando o ID não existir
         Mockito.when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+        // Configura o mock para retornar o DTO atualizado quando o ID existir
+        Mockito.when(service.update(eq(existingId), any())).thenReturn(productDTO);
+
+        // Configura o mock para lançar exceção ao tentar atualizar um ID inexistente
+        Mockito.when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception{
+
+        // ARRANGE: instancie os objetos necessários
+        // Converte o objeto ProductDTO para uma String JSON
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        // ACT: execute as ações necessárias
+        // Realiza uma requisição PUT para /products/{id}
+        // .content(jsonBody): Define o corpo da requisição com o JSON criado
+        // .contentType(MediaType.APPLICATION_JSON): Define o cabeçalho Content-Type como application/json
+        // .accept(MediaType.APPLICATION_JSON): Define que a resposta esperada é JSON
+        ResultActions result = mockMvc.perform(put("/products/{id}",existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // ASSERT: declare o que deveria acontecer (resultado esperado)
+        // Verifica se o status da resposta é 200 OK
+        result.andExpect(status().isOk());
+        // Verifica se os campos do JSON existem na resposta
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+        // ARRANGE: instancie os objetos necessários
+        // Converte o objeto ProductDTO para uma String JSON
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        
+        // ACT: execute as ações necessárias
+        // Realiza uma requisição PUT para /products/{id} com um ID inexistente
+        ResultActions result = mockMvc.perform(put("/products/{id}",nonExistingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // ASSERT: declare o que deveria acontecer (resultado esperado)
+        // Verifica se o status da resposta é 404 Not Found
+        result.andExpect(status().isNotFound());
     }
 
     @Test
